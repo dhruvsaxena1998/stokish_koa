7029a1d8b5aac8b3ddfba4f7c096401fa15d53e5
dhruvsaxena1998 <saxenadhruv1927@gmail.com>
2021-09-15 02:26:21 +0530

    chore(depedency-injection): fixed di for user-repository

--- src/api/repositories/user.repository.ts
+++ src/api/repositories/user.repository.ts
@@ -1,14 +1,12 @@
-import { Connection, Repository } from "typeorm";
+import { Repository } from "typeorm";
 import { SharedRepository } from "./shared.repository";
 import { UsersEntity } from "../../entities";
-import { Container, Dependencies } from "../../injection";
-
-const database: Connection = Container.resolve("database");
-const entity = database.getRepository(UsersEntity);
+import { Dependencies } from "../../injection";
 
 export class UserRepository extends SharedRepository<UsersEntity> {
   private readonly _userEntity: Repository<UsersEntity>;
-  constructor() {
+  constructor({ database }: Dependencies) {
+    const entity = database.getRepository(UsersEntity);
     super(entity);
     this._userEntity = entity;
   }
